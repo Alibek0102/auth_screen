a19a6642e36390fc6c4efe1f147bcb9d42186da1
Alibek Yessetov <alibek.yesetov@bk.ru>
2025-05-13 07:59:07 +0500

    add loader

--- lib/futures/category_details/screen/category_details_screen.dart
+++ lib/futures/category_details/screen/category_details_screen.dart
@@ -7,6 +7,7 @@ import 'package:auth_screen/futures/home/common/products/product_item.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
 
 @RoutePage()
 class CategoryDetailsScreen extends StatelessWidget {
@@ -16,6 +17,8 @@ class CategoryDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       body: BlocBuilder<CategoryDetailsBloc, CategoryDetailsState>(
         bloc: getIt.get<CategoryDetailsBloc>()
@@ -29,54 +32,72 @@ class CategoryDetailsScreen extends StatelessWidget {
                 },
               ),
               ...state.when(
-                  initial: () => [
-                        const SliverToBoxAdapter(
-                          child: Text('initial'),
+                initial: () => [
+                  const SliverToBoxAdapter(
+                    child: Text('initial'),
+                  )
+                ],
+                loader: () => [
+                  const SliverFillRemaining(
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  )
+                ],
+                loaded: (products) => [
+                  SliverToBoxAdapter(
+                    child: 15.height,
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    sliver: CategoryTitle(title: model, count: products.length),
+                  ),
+                  SliverToBoxAdapter(
+                    child: 15.height,
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    sliver: SliverGrid.builder(
+                        itemCount: products.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisSpacing: 20,
+                                mainAxisSpacing: 20,
+                                crossAxisCount: 2,
+                                mainAxisExtent: 281),
+                        itemBuilder: (BuildContext context, int index) {
+                          return ProductItem(productEntity: products[index]);
+                        }),
+                  ),
+                  SliverToBoxAdapter(
+                    child: 34.height,
+                  ),
+                ],
+                error: () => [
+                  SliverFillRemaining(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        (screenSize.height * 0.2).toInt().height,
+                        Lottie.asset('assets/lotties/productError.json',
+                            width: screenSize.width,
+                            height: screenSize.width / 2),
+                        const Text(
+                          'Ошибка загрузки',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        15.height,
+                        const Text(
+                          'Не удалось загрузить продукты.\nПожалуйста, попробуйте снова.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.grey),
                         )
                       ],
-                  loader: () => [
-                        const SliverToBoxAdapter(
-                          child: Center(
-                            child: CircularProgressIndicator(),
-                          ),
-                        )
-                      ],
-                  loaded: (products) => [
-                        SliverToBoxAdapter(
-                          child: 15.height,
-                        ),
-                        SliverPadding(
-                          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                          sliver: CategoryTitle(
-                              title: model, count: products.length),
-                        ),
-                        SliverToBoxAdapter(
-                          child: 15.height,
-                        ),
-                        SliverPadding(
-                          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                          sliver: SliverGrid.builder(
-                              itemCount: products.length,
-                              gridDelegate:
-                                  const SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisSpacing: 20,
-                                      mainAxisSpacing: 20,
-                                      crossAxisCount: 2,
-                                      mainAxisExtent: 281),
-                              itemBuilder: (BuildContext context, int index) {
-                                return ProductItem(
-                                    productEntity: products[index]);
-                              }),
-                        ),
-                        SliverToBoxAdapter(
-                          child: 34.height,
-                        ),
-                      ],
-                  error: () => [
-                        const SliverToBoxAdapter(
-                          child: Text('Error'),
-                        )
-                      ]),
+                    ),
+                  )
+                ],
+              ),
             ],
           );
         },
