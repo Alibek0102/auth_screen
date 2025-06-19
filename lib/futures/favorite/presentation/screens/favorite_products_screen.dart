89e249c4387cd58880f6953dc10edf9e1aa2d29c
Alibek Yessetov <alibek.yesetov@bk.ru>
2025-06-19 20:29:45 +0500

    favorite products complete

--- lib/futures/favorite/presentation/screens/favorite_products_screen.dart
+++ lib/futures/favorite/presentation/screens/favorite_products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auth_screen/core/di/service_locator.dart';
 import 'package:auth_screen/elements/custom_app_bar.dart';
 import 'package:auth_screen/futures/favorite/presentation/blocs/favorite_cubit.dart';
+import 'package:auth_screen/futures/home/common/products/product_item.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,9 +20,28 @@ class FavoriteProductsScreen extends StatelessWidget {
         },
       ),
       body: BlocBuilder<FavoriteCubit, FavoriteState>(
-        bloc: getIt.get<FavoriteCubit>()..loadFavoriteProducts(),
+        bloc: getIt.get<FavoriteCubit>(),
         builder: (context, state) {
-          return Center();
+          return GridView.builder(
+              padding:
+                  const EdgeInsets.only(right: 24.0, left: 24.0, bottom: 50.0),
+              itemCount: state.products.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 20.0,
+                  crossAxisSpacing: 20.0,
+                  childAspectRatio: 0.7),
+              itemBuilder: (BuildContext context, int index) {
+                return ProductItem(
+                  productEntity: state.products[index],
+                  availableInCart: true,
+                  onAddToCart: () {
+                    getIt
+                        .get<FavoriteCubit>()
+                        .appendFavoriteProduct(product: state.products[index]);
+                  },
+                );
+              });
         },
       ),
     );
